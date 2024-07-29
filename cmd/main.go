d5e31c46b13560cfc10f2037901748d4503ad29d
Rajanikant <rajanikant@itnium.com>
2024-07-29 13:09:10 +0530

    initial

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"go-trainings/cmd/api"
+	"go-trainings/config"
+	"go-trainings/db"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	cfg := mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	db, err := db.NewMySqlStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("DB: Successfully connected!")
+}
